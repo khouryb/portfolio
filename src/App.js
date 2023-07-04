e091dd23eb43ba374041ae88eebb1b5110b9da0f
Benjamin Khoury <khourybenjamin@gmail.com>
2023-07-04 21:45:55 +0100

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl underline">Hello</h1>
+      <NavBar />
     </div>
   );
 }
